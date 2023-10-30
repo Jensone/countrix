d52a4be558500392e238f90a8f811fc7c7bf8e22
Jensone <hello@jensone.com>
2023-10-30 15:36:26 +0100

    Premier commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
+
+
+// http://localhost:5173/vite.svg
+// http://localhost:5173/src/assets/react.svg
